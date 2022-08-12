c76df44902f615866580074bcc5e875d11f62307
kevin kinyua <51110607+sirkeygun@users.noreply.github.com>
2022-08-12 15:00:36 +0300

    date posted
    Co-authored-by: Ephantus Wambui <Ephantus-Wambui@users.noreply.github.com>

--- components/PostWidgets.jsx
+++ components/PostWidgets.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import moment from 'moment';
+import Link from 'next/link';
 
 const PostWidgets = () => {
   return (
