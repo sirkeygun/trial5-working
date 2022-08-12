51b37a4d283605e42686ccaff53edbbf0ede9d91
kevin kinyua <51110607+sirkeygun@users.noreply.github.com>
2022-08-12 15:01:30 +0300

    date

--- services/index.js
+++ services/index.js
@@ -35,3 +35,24 @@ export const getPosts = async () => {
   const result = await request(graphqlAPI, query);
   return result.postsConnection.edges;
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
